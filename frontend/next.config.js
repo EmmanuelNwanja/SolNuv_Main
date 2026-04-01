ac30eb2003838713cda09b3cdab45a3d8f2675fd
Wanja <emmanuelnwanja@gmail.com>
2026-04-01 13:02:38 +0100

    security: remove unsafe-eval from CSP script-src (not needed in Turbopack production build)

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -36,7 +36,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://static.cloudflareinsights.com https://js.paystack.co",
+              "script-src 'self' 'unsafe-inline' https://static.cloudflareinsights.com https://js.paystack.co",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://lh3.googleusercontent.com https://avatars.githubusercontent.com https://solnuv.com",
